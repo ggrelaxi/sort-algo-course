7d82f67a3377f60cab417164d6a2b3efc8340005
aleksandr.smirnov <ggrelaxi@gmail.com>
2025-05-23 16:45:45 +0300

    knuth sequence implementation

--- insertion_sort.py
+++ insertion_sort.py
@@ -20,3 +20,22 @@ def InsertionSortStep(array: List[int], step: int, i: int):
             array[prev_position - step] = temp
 
             prev_position = prev_position - step
+
+
+def KnuthSequence(array_size: int):
+    if array_size == 0:
+        return []
+
+    temp_sequence = []
+
+    base = 1
+
+    while base <= array_size:
+        temp_sequence.append(base)
+        base = 3 * base + 1
+
+    sequence = []
+    for i in range(0, len(temp_sequence)):
+        sequence.append(temp_sequence[len(temp_sequence) - i - 1])
+
+    return sequence
